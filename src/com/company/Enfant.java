e80ccbb9ba4b98ae9866f188ecf7a16b80a69a72
jolyqr <jolyqr@gmail.com>
2020-07-08 23:54:28 +0200

    first commit

---
+++ src/com/company/Enfant.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Enfant extends Personne {
+    public void demenage(){
+        System.out.println("Enfant déménage");
+    }
+}
---
+++ src/com/company/Personne.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Personne {
+    public int age;
+    private String ville="Paris";
+    public String sexe="male";
+    public String nom="Paul";
+
+    public void demenage(){
+        System.out.println("Personne déménage");
+    }
+}
